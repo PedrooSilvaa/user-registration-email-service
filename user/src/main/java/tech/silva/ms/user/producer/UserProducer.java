1182e4622eec93f94f5ab8fd65c850de88d22980
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2025-02-18 10:33:13 -0300

    feat: create user producer

---
+++ user/src/main/java/tech/silva/ms/user/producer/UserProducer.java
@@ -0,0 +1,30 @@
+package tech.silva.ms.user.producer;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import tech.silva.ms.user.dto.EmailDto;
+import tech.silva.ms.user.entity.UserEntity;
+
+@Component
+public class UserProducer {
+
+    final RabbitTemplate rabbitTemplate;
+
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value("${broker.queue.email.name}")
+    private String routingKey;
+
+    public void publishMessageEmail(UserEntity userEntity){
+        var emailDto = new EmailDto();
+        emailDto.setUserId(userEntity.getId());
+        emailDto.setEmailTo(userEntity.getEmail());
+        emailDto.setSubject("Cadastro realizado com sucesso!");
+        emailDto.setText(userEntity.getName() + ", seja bem vindo(a)! \n Agradecemos o seu cadastro, aproveite agora todos os recursos da nossa plataforma!");
+
+        rabbitTemplate.convertAndSend("", routingKey, emailDto);
+    }
+}
