2424aa4c0d392b31afe7d2acc4573de62bfef981
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2025-02-18 10:40:27 -0300

    feat: configure and send message

--- user/src/main/java/tech/silva/ms/user/service/UserService.java
+++ user/src/main/java/tech/silva/ms/user/service/UserService.java
@@ -3,19 +3,24 @@ package tech.silva.ms.user.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tech.silva.ms.user.entity.UserEntity;
+import tech.silva.ms.user.producer.UserProducer;
 import tech.silva.ms.user.repository.UserRepository;
 
 @Service
 public class UserService {
 
     final UserRepository userRepository;
+    final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserEntity save(UserEntity userEntity){
-        return userRepository.save(userEntity);
+        userEntity =  userRepository.save(userEntity);
+        userProducer.publishMessageEmail(userEntity);
+        return userEntity;
     }
 }
